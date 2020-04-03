01e2f57d20cc38ae701b93441ced0cf32946e209
Celso Crivelaro <celso.crivelaro@revelo.com.br>
2020-04-03 20:08:40 -0300

    Corrigindo castings

--- senac-poo/src/br/senac/jogo/Jogo.java
+++ senac-poo/src/br/senac/jogo/Jogo.java
@@ -16,26 +16,27 @@ public class Jogo {
 	// Adaptador => Objeto que liga com infraestrutura de software
 	
 	public static void main(String[] args) throws InterruptedException {
+		Combatente vencedor;
+		
 		// orc
 		// dragao
 		Combatente inimigo1 = new Orc();
 		Combatente inimigo2 = new Anjo();
-		System.out.println("Iniciando jogo com os inimigos:");
-		System.out.println(inimigo1.getNome());
-		System.out.println(inimigo2.getNome());
-		System.out.println("\n");
-
-	
-		Batalha batalha = new Batalha(inimigo1, inimigo2);
-		Combatente vencedor = batalha.lutar();
-		System.out.println("Batalha normal => Vecendor: " + vencedor.getNome());
+//		System.out.println("Iniciando jogo com os inimigos:");
+//		System.out.println(inimigo1.getNome());
+//		System.out.println(inimigo2.getNome());
+//		System.out.println("\n");
+//	
+//		Batalha batalha = new Batalha(inimigo1, inimigo2);
+//		vencedor = batalha.lutar();
+//		System.out.println("Batalha normal => Vecendor: " + vencedor.getNome());
+//		
+//		Batalha batalhaViciada = new BatalhaViciada(inimigo1, inimigo2);
+//		vencedor = batalhaViciada.lutar();
+//		System.out.println("Batalha Viciada => Vecendor: " + vencedor.getNome());
 		
-		Batalha batalhaViciada = new BatalhaViciada(inimigo1, inimigo2);
-		vencedor = batalhaViciada.lutar();
-		System.out.println("Batalha Viciada => Vecendor: " + vencedor.getNome());
-		
-		Combatente inimigo3 = new FuriaDaNoite();
-		Batalha batalhaDosAres = new BatalhaViciada(inimigo3, inimigo2);
+		ISerVoador inimigo3 = new FuriaDaNoite();
+		BatalhaDosAres batalhaDosAres = new BatalhaDosAres(inimigo3, (ISerVoador) inimigo2);
 		vencedor = batalhaDosAres.lutar();
 		System.out.println("Batalha dos Ares => Vecendor: " + vencedor.getNome());
 		
